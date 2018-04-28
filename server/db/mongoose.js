3fc04d1272ffa383215e7a1e37af8d383d9966cc
River Hermsen <riverhermsen@hotmail.nl>
2018-04-28 20:54:44 +0200

    Made app ready for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,13 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var urlMongoDB;
+if (process.env.PORT) {
+  var urlMongoDB = "mongodb://admin:admin@ds117109.mlab.com:17109/todo-node"
+} else {
+  var urlMongoDB = "mongodb://localhost:27017/TodoApp";
+}
+mongoose.connect(urlMongoDB);
 
 module.exports = {mongoose};
