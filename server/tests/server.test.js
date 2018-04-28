54825f4a54092f4a2570d56f652318e7b11f1563
River Hermsen <riverhermsen@hotmail.nl>
2018-04-28 15:55:21 +0200

    Added tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -8,10 +8,20 @@ const {
   Todo
 } = require('./../models/todo');
 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}]
+
 beforeEach((done) => {
   Todo.remove({}).then(() => {
-    done();
-  });
+    return Todo.insertMany(todos, (err, docs) => {
+      if (err) {
+        return done(err);
+      }
+    });
+  }).then(() => done())
 });
 
 describe('POST /todos', () => {
@@ -32,7 +42,9 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({
+          text
+        }).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -55,9 +67,21 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       })
   });
 });
+
+describe('GET /todos', () => {
+  it('Should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
+})
